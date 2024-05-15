3b1442dfed278f9bf7d66734a625142e7199ce3c
makalaosiggins <wrxmakala@icloud.com>
2024-05-15 07:40:35 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', function() {
+        console.log('Button clicked!');
+        // Add any functionality you want to happen when the button is clicked
+    });
 }
